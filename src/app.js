385424f37c5285a3435f307632d7c521a352cd12
Jon Preece <jonpreece@hotmail.co.uk>
2018-12-28 19:24:42 +0000

    Added image

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ import React from "react";
 import { hot } from "react-hot-loader/root";
 import Counter from "./counter.js";
 
+import img from "./img/jonpreece-square.png";
+
 function App() {
-  return <Counter count={10} />;
+  return (
+    <>
+      <img src={img} alt="Jon Preece" />
+      <Counter count={10} />
+    </>
+  );
 }
 
 export default hot(App);
